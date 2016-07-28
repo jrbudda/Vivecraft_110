446bb8a6b1f511fa11651b491d1cc6f43ef675b1
jrbudda <jrbudda@gmail.com>
2016-07-28 19:37:59 -0400

    fix boats, mr cam w/ shaders

--- installer/Installer.java
+++ installer/Installer.java
@@ -899,8 +899,7 @@ public class Installer extends JPanel  implements PropertyChangeListener
             }
 
             if (checkedRedists && !redistSuccess) {
-                monitor.close();
-                return null;
+				JOptionPane.showMessageDialog(null, "Could not download VC++ Redist. Game might not work.", "Warning", JOptionPane.INFORMATION_MESSAGE);
             }
 
             finalMessage = "Failed: Couldn't download Optifine. ";
@@ -1234,7 +1233,7 @@ public class Installer extends JPanel  implements PropertyChangeListener
             logoLabel.setAlignmentX(CENTER_ALIGNMENT);
             logoLabel.setAlignmentY(CENTER_ALIGNMENT);
             logoLabel.setSize(image.getWidth(), image.getHeight());
-            if (!QUIET_DEV && false)	// VIVE - hide oculus logo
+            if (!QUIET_DEV)	// VIVE - hide oculus logo
 	            logoSplash.add(logoLabel);
         } catch (IOException e) {
         } catch( IllegalArgumentException e) {
@@ -1417,8 +1416,8 @@ public class Installer extends JPanel  implements PropertyChangeListener
         
         
         this.add(Box.createVerticalGlue());
-        JLabel github = linkify("Vivecraft is open source. find it on Github","https://github.com/jrbudda/minecrift/releases","Vivecraft Github");
-        JLabel wiki = linkify("Vivecraft wiki page","https://www.reddit.com/r/Vive/wiki/minecrift_for_vive","Reddit Vivecraft Wiki");
+        JLabel github = linkify("Vivecraft is open source. find it on Github","https://github.com/jrbudda/Vivecraft_110","Vivecraft 1.10 Github");
+        JLabel wiki = linkify("Vivecraft home page","http://www.vivecraft.org","Vivecraft Home");
         JLabel donate = linkify("If you think Vivecraft is awesome, please consider donating.","https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=JVBJLN5HJJS52&lc=US&item_name=jrbudda&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted)","jrbudda's Paypal");
         JLabel optifine = linkify("Vivecraft includes OptiFine for performance. Consider donating to them as well.","http://optifine.net/donate.php","http://optifine.net/donate.php");
 
