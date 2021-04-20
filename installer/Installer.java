d0ff4d47159396197559971917586e04645a3cda
Techjar <tecknojar@gmail.com>
2021-04-20 04:39:10 -0400

    Update Installer.java

--- installer/Installer.java
+++ installer/Installer.java
@@ -80,7 +80,7 @@ public class Installer extends JPanel  implements PropertyChangeListener
     private String[] forgeVersions = null;
     private boolean forgeVersionInstalled = false;
 	private static String FULL_FORGE_VERSION = MINECRAFT_VERSION + "-" + FORGE_VERSION;
-    private String forge_url = "http://maven.minecraftforge.net/maven/net/minecraftforge/forge/" + FULL_FORGE_VERSION + "/forge-" + FULL_FORGE_VERSION + "-installer.jar";
+    private String forge_url = "http://maven.minecraftforge.net/net/minecraftforge/forge/" + FULL_FORGE_VERSION + "/forge-" + FULL_FORGE_VERSION + "-installer.jar";
 	private File forgeInstaller;
     private JTextField selectedDirText;
     private JLabel infoLabel;
@@ -664,7 +664,7 @@ public class Installer extends JPanel  implements PropertyChangeListener
 					
 				FULL_FORGE_VERSION = MINECRAFT_VERSION + "-" + FORGE_VERSION;
 				forgeInstaller = new File(tempDir + "/forge-" + FULL_FORGE_VERSION + "-installer.jar");
-				forge_url = "http://maven.minecraftforge.net/maven/net/minecraftforge/forge/" + FULL_FORGE_VERSION + "/forge-" + FULL_FORGE_VERSION + "-installer.jar";
+				forge_url = "http://maven.minecraftforge.net/net/minecraftforge/forge/" + FULL_FORGE_VERSION + "/forge-" + FULL_FORGE_VERSION + "-installer.jar";
 
 				if( targetDir.exists() ) {
 					File ForgeDir = new File( targetDir, "libraries"+File.separator+"net"+File.separator+"minecraftforge"+File.separator+"forge");
